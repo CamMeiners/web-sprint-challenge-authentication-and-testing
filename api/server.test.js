60f3a8778a2060db4d97137f8c820dac16429b3b
CamMeiners <cammeiners@gmail.com>
2021-11-12 16:06:53 -0700

    finished project

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,63 @@
-// Write your tests here
+const request = require('supertest');
+const db = require('../data/dbConfig');
+const server = require('./server');
+const Jokes = require('./jokes/jokes-data');
+
+const user1 = { username: 'admin1', password: 'password' };
+
+beforeAll(async () => {
+  await db.migrate.rollback();
+  await db.migrate.latest();
+});
+beforeEach(async () => {
+  await db.seed.run();
+});
+afterAll(async () => {
+  await db.destroy();
+});
+
 test('sanity', () => {
   expect(true).toBe(true);
+});
+
+describe('User endpoint testing', () => {
+  beforeEach(async () => {
+    await request(server).post('/api/auth/register').send(user1);
+  });
+  test('Able to create a new user', async () => {
+    let users;
+    users = await db('users');
+    expect(users).toHaveLength(4);
+  });
+
+  test('Able to login', async () => {
+    const res = await request(server)
+      .post('/api/auth/login')
+      .send({ username: 'admin1', password: 'password' });
+    expect(res.body.message).toMatch(/admin1 is back/i);
+  });
+  test('Not able to login with invalid creds', async () => {
+    const res = await request(server)
+      .post('/api/auth/login')
+      .send({ username: 'admin1', password: 'thisIsNotThePassword' });
+    expect(res.body.message).toMatch(/invalid credentials/i);
+  });
+});
+
+describe('Joke endpoint testing', () => {
+  test('Jokes are restricted behind login', async () => {
+    const res = await request(server).get('/api/jokes');
+    expect(res.body.message).toMatch(/token required/i);
+  });
+  test('Jokes are accessible after login', async () => {
+    await request(server).post('/api/auth/register').send(user1);
+    const res = await request(server)
+      .post('/api/auth/login')
+      .send({ username: 'admin1', password: 'password' });
+    const token = res.body.token;
+    const jokes = await request(server)
+      .get('/api/jokes')
+      .set({ Authorization: token });
+    expect(jokes.body[0].joke).toEqual(Jokes[0].joke);
+  });
 });
--- api/users/users-model.js
+++ api/users/users-model.js
@@ -5,30 +5,19 @@ module.exports = {
   findById,
   add,
 };
-/**
-    resolves to an ARRAY with all users, each user having { user_id, username }
-   */
+
 function find() {
-  return db('users').select('user_id', 'username');
+  return db('users').select('id', 'username');
 }
 
-/**
-    resolves to an ARRAY with all users that match the filter condition
-   */
 function findBy(filter) {
   return db('users').where(filter);
 }
 
-/**
-    resolves to the user { user_id, username } with the given user_id
-   */
 function findById(id) {
   return db('users').select('id', 'username').where({ id }).first();
 }
 
-/**
-    resolves to the newly inserted user { user_id, username }
-   */
 async function add(user) {
   const [id] = await db('users').insert(user);
   return findById(id);
